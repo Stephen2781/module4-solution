7a1437605430d7f80745bbb46caa162d89bc478a
Stephen2781 <136972094+Stephen2781@users.noreply.github.com>
2024-01-09 12:23:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-(function () {
+(
+  function(){
   
-    var names = ["Stephen", "Billi", "John", "Jesumbo", "Jacob", "Amodu", "Dumebi"];
-  
-    for (var i = 0; i < names.length; i++) {
-  
-       var firstLetter = names[i].charAt(0).toLowerCase();
-  
-      if (firstLetter === 'j') {
-         byeSpeaker.speak(names[i]);
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+   
+    for ( var i = 0; i < names.length; i++) {
+
+         var firstLetter =names[i].charAt(0);
+      
+         if (firstLetter.toLocaleLowerCase() == "j") {
+        byeSpeaker.speak(names[i]);
       } else {
         helloSpeaker.speak(names[i]);
       }
     }
-  })();
+
+  }
+)();
